55737dcd83e4b9a262f32af1e2e850b10a7f8173
alireza <khoshgoftar@gmail.com>
2012-11-10 21:23:41 -0500

    / ‘Utilities/DeriveCG.m’

--- Utilities/DeriveCG.m
+++ Utilities/DeriveCG.m
@@ -1,12 +1,13 @@
-function  CG= DeriveCG(X, R, R2);
-% Derives the connectivity graph from the locations
-%   X = location matrix: d x N
-%   R = trnasmission range
-% Returns N x N connectivity graph
-if nargin < 3
-    R2 = R;
-else
-    R = R2;
-end
+function  CG= DeriveCG(X, Mode, R);
+%DERIVECG Derive connectivity graph of a mobility trace.
+%   CG= DERIVECG(X, Mode, R) derives the connectivity graph in the form of
+%   a NxN matrix from a given 2xN matrix of locations and a corresponding
+%   transmission range.
+%   Inputs:
+%   *X : location matrix: d x N
+%   *Mode: Set it to 1. It will be used to implement furture options.
+%   *R : trnasmission range
+%   Output: 
+%   *CG: N x N connectivity graph
     CG = squareform(pdist(X') <= R);
 end
