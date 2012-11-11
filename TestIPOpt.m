a07cd9a4e4ecc3e3c7d297375c1377f99e3f0b97
alireza <khoshgoftar@gmail.com>
2012-11-10 21:34:41 -0500

    / ‘TestIPOpt.m’

--- TestIPOpt.m
+++ TestIPOpt.m
@@ -9,29 +9,32 @@ disp('To recreate the mobility traces in the paper choose one of the following t
 dis('1 = RandomWaypoint')
 dis('2 = Reference Point Group Mobility')
 dis('3 = Self Similar Least Action Walks')
-strResponse = input('Choose your synthetic trace: ', 's')
-%% RWP
-InputFile = './Inputs/RWPLinks.one'
-Options.OutputFile = './Results/RWPInferredNIEps.one'
-Options.epsIn = 0.2;
-Options.epsOut = 1;
-Options.Vm = 10;
-
-%% RPGM
-% InputFile = './Inputs/RPGMLinks.one'
-% Options.OutputFile = './Results/RPGMInferredNIEps.one'
-% Options.epsIn = 0.2;
-% Options.epsOut = 1;
-% Options.Vm = 10;
-
-%% SLAW
-% InputFile = './Inputs/SLAWLocs.one'
-% Options.OutputFile = './Results/SLAWInferredNIEps.one'
-% Options.InputIsContacts = false;
-% Options.epsIn = 0.2;
-% Options.epsOut = 1;
-% Options.Vm = 33;
-
+Response = input('Choose your synthetic trace: ')
+if Response == 1
+    % RWP
+    InputFile = './Inputs/RWPLinks.one'
+    Options.OutputFile = './Results/RWPInferredNIEps.one'
+    Options.epsIn = 0.2;
+    Options.epsOut = 1;
+    Options.Vm = 10;
+elseif Response == 2
+    % RPGM
+    InputFile = './Inputs/RPGMLinks.one'
+    Options.OutputFile = './Results/RPGMInferredNIEps.one'
+    Options.epsIn = 0.2;
+    Options.epsOut = 1;
+    Options.Vm = 10;
+elseif Response == 3
+    % SLAW
+    InputFile = './Inputs/SLAWLocs.one'
+    Options.OutputFile = './Results/SLAWInferredNIEps.one'
+    Options.InputIsContacts = false;
+    Options.epsIn = 0.2;
+    Options.epsOut = 1;
+    Options.Vm = 33;
+else
+    disp('Only valid Options are 1,2, and 3')
+end
 
 %% MAIN RUN
 % [X TimeSequence Nodes Box]= ImportONE('../InputStore/RWPLocs1.one');
